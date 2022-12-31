3fe51feb2df7e443e8c203301a4b20db33403f5a
Sam Turner <98767222+stmio@users.noreply.github.com>
2022-12-31 15:16:31 +0000

    fix(meta): update website info

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "My personal website.";
+export const SITE_TITLE = "Sam Turner (stmio)";
 export const SITE_DESCRIPTION = "Welcome to my website!";
+export const HOMEPAGE_URL = "https://stmio.net/";
+export const GITHUB_PROFILE_URL = "https://github.com/stmio/";
+export const TWITTER_PROFILE_URL = "https://twitter.com/stm_io/";
