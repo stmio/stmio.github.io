1a4129244816fa7eae735bff0e740d348c09af73
Sam Turner <98767222+stmio@users.noreply.github.com>
2023-02-20 20:07:15 +0000

    perf(deps): add deps to improve site performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,25 @@
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import { remarkReadingTime } from "./src/remark-reading-time.mjs";
-
 import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
+import image from "@astrojs/image";
+import prefetch from "@astrojs/prefetch";
+import compress from "astro-compress";
+import critters from "astro-critters";
+import { remarkReadingTime } from "./src/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://stmio.net",
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    prefetch(),
+    compress(),
+    critters(),
+  ],
   markdown: {
     remarkPlugins: [remarkReadingTime],
     extendDefaultPlugins: true,
