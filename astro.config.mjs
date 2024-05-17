de33cc6a940fe7c6ca864d34b304c42e53e8b8ab
Sam Turner <98767222+stmio@users.noreply.github.com>
2024-05-17 20:42:01 +0100

    feat!: upgrade astro to v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
-import image from "@astrojs/image";
-import prefetch from "@astrojs/prefetch";
 import compress from "astro-compress";
 import critters from "astro-critters";
 import { remarkReadingTime } from "./src/remark-reading-time.mjs";
@@ -10,16 +8,8 @@ import { remarkReadingTime } from "./src/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
   site: "https://stmio.net",
-  integrations: [
-    mdx(),
-    sitemap(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    prefetch(),
-    critters(),
-    compress(),
-  ],
+  integrations: [mdx(), sitemap(), critters(), compress()],
+  prefetch: true,
   markdown: {
     remarkPlugins: [remarkReadingTime],
     extendDefaultPlugins: true,
