e08ee9d8c1cee33cca58f156d1b6f79bd4ab3d96
Sam Turner <98767222+stmio@users.noreply.github.com>
2025-04-10 16:56:50 +0100

    Migrate to the Astro Micro theme
    Signed-off-by: Sam Turner <98767222+stmio@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,19 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
-import compress from "astro-compress";
-import critters from "astro-critters";
-import { remarkReadingTime } from "./src/remark-reading-time.mjs";
+import pagefind from "astro-pagefind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://stmio.net",
-  integrations: [mdx(), sitemap(), critters(), compress({ SVG: false })],
-  prefetch: true,
+  site: "https://astro-micro.vercel.app",
+  integrations: [sitemap(), mdx(), pagefind()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   markdown: {
-    remarkPlugins: [remarkReadingTime],
-    extendDefaultPlugins: true,
+    shikiConfig: {
+      theme: "css-variables",
+    },
   },
 });
